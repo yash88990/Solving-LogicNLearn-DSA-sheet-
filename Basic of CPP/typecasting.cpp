903e046f0ba0a7e64feb75916dcc73134df9179e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-15 05:43:59 +0530

    Create typecasting.cpp

---
+++ Basic of CPP/typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+    // Typecast and Print
+    int ans = int(d);
+    cout<<ans<<endl;
+    
+    
+
+    return 0;
+}
