39f0b0bf309693229c04e316732c95f3b2f23661
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-18 07:15:52 +0530

    Create count_total_nodes.cpp

---
+++ Binary Tree/count_total_nodes.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root)return 0;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
+
+//method 2 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root  , int &count){
+        if(!root)return;
+        inorder(root->left , count);
+        count++;
+        inorder(root->right , count);
+    }
+    int countNodes(TreeNode* root) {
+        if(!root)return 0;
+        int count =0;
+        inorder(root , count);
+        return count;
+    }
+};
