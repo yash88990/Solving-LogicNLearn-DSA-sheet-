50a60e1cb491a772c4d3c739ac2b00af24dc2d39
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-04 14:31:22 +0530

    Create identify-smith-numbers.cpp

---
+++ Maths/identify-smith-numbers.cpp
@@ -0,0 +1,43 @@
+int digitsum(int n){
+    int sum = 0;
+    while(n != 0){
+        int digit = n % 10;
+        sum += digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool prime(int n){
+  if (n <= 1) return false; 
+  if (n <= 3) return true;
+  if (n % 2 == 0 || n % 3 == 0) return false;
+  for (int i = 5; i*i <= n; i += 6)
+      if (n % i == 0 || n % (i+2) == 0) return false;
+  return true;
+}
+int solve(int n) {
+    if(prime(n))return 0;
+    int originalsum = digitsum(n);
+    int factorsum = 0;
+    int num = n;
+    //factor out 2s
+    while (num % 2 == 0) {
+        factorsum += digitsum(2);
+        num /= 2;
+    }
+    // Factor out odd primes starting from 3
+    for (int i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            factorsum += digitsum(i);
+            num /= i;
+        }
+    }
+    if (num > 1) {
+        factorsum += digitsum(num);
+    }
+    
+    // Compare the two sums
+    return (originalsum == factorsum) ? 1 : 0;
+
+}
