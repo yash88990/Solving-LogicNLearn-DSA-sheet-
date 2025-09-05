ccf9125baaca6f8e072799fb919fdea31e30c69b
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-05 14:08:43 +0530

    Update identify-smith-numbers.cpp

--- Maths/identify-smith-numbers.cpp
+++ Maths/identify-smith-numbers.cpp
@@ -9,12 +9,10 @@ int digitsum(int n){
 }
 
 bool prime(int n){
-  if (n <= 1) return false; 
-  if (n <= 3) return true;
-  if (n % 2 == 0 || n % 3 == 0) return false;
-  for (int i = 5; i*i <= n; i += 6)
-      if (n % i == 0 || n % (i+2) == 0) return false;
-  return true;
+   if(n <= 1)return false;
+   for(int i = 2 ; i * i < n ; i++)
+        if(n % i == 0)return false;
+   return true;
 }
 int solve(int n) {
     if(prime(n))return 0;
